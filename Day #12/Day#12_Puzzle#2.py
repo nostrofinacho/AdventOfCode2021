461c40f9fdf60165e173cf8fcf7e3d1a8d898232
Marko Kršić <nostro.finacho@gmail.com>
2021-12-12 18:23:54 +0100

    Day #12, Puzzle #2

--- Day #12/Day#12_Puzzle#2.py
+++ Day #12/Day#12_Puzzle#2.py
@@ -3,11 +3,12 @@
 # ~~~~~ The Pathfinder ~~~~~
 # ~~~~~ I'll be your spotter ~~~~~
 
-import copy
+import copy, collections
 
 # Mapping -> format is [[beggining_area, end area], [beggining_area, end area], ...]
 bag_end = []
 small_caves = []    # List of small caves
+small_big_caves = []    # The small caves that could be visited twice
 
 
 # Recreates a path into a string
@@ -30,14 +31,14 @@ def search_around_path(path):
 
 # Expands a node
 def expand(n):
-    return [(area, n[1] + 1, n, copy.deepcopy(n[3])) for area in search_around_path(n[0]) if area not in n[3]]
+    return [[area, n[1] + 1, n, copy.deepcopy(n[3]), n[4]] for area in search_around_path(n[0]) if area not in n[3]]
 
 
 # Returns list of paths
-# Node format -> (area_name, path_depth, parent_node, visited_small_caves_list)
-def pathfinder(s0, goal):
+# Node format -> (area_name, path_depth, parent_node, visited_small_caves_list, big_small_cave)
+def pathfinder(s0, goal, small_big_cave):
     # Current area queue
-    open = [(s0, 0, None, [])]
+    open = [[s0, 0, None, [], '']]
 
     # Nodes that end a path
     closed = []
@@ -52,31 +53,42 @@ def pathfinder(s0, goal):
 
         # Remember the small cave
         if n[0] in small_caves:
-            n[3].append(n[0])
+            if n[0] == small_big_cave and n[4] == '':
+                n[4] = n[0]
+            else:
+                n[3].append(n[0])
 
         # Search for nearby areas
         for m in expand(n):
-            open.append(m)
-
+            #open.append(m)     # Breadth-first search
+            open.insert(0, m)   # Depth-first search
     # Return end nodes
     return closed
 
 
 # Input reading
-with open("Day#12_Puzzle#1_Input.txt") as input_file:
+with open("Day#12_Puzzle#2_Input.txt") as input_file:
     for line in input_file:
         # Mapping
         bag_end.append(line.strip().split('-'))
 
         # Remember the small caves
         for area in line.strip().split('-'):
-            if area.islower():
+            if area.islower() and area not in small_caves:
                 small_caves.append(area)
+                if area != "start" and area != "end":
+                    small_big_caves.append(area)
 
-# Find and print the paths
-paths = pathfinder("start", "end")
-for path in paths:
-    print(get_path(path))
+# Find the paths, and count all the paths throughout switching the small-big cave
+paths = []
+paths_s = []
+# A legitimate small cave is once given the two-time visit
+for small_big_cave in small_big_caves:
+    some_paths = pathfinder("start", "end", small_big_cave)
+    paths += some_paths
+    # Stringing
+    for path in some_paths:
+        paths_s.append(get_path(path))
 
-# Task solution
-print("\nThere are", len(paths), "possible paths.")
+# Solution
+print("There are", len(collections.Counter(paths_s).keys()), "possible unique paths.")
