ec645c897b7e27c70ff670981aa48e2d47abbc80
Marko Kršić <nostro.finacho@gmail.com>
2021-12-22 09:12:34 +0100

    Day #22, Puzzle #1, Naive version

---
+++ Day #22/Day#22_Puzzle#1.py
@@ -0,0 +1,36 @@
+# Advent of Code
+# ~~~~~ Actor Boot Again ~~~~~
+
+
+# Turns cuboid on or off -> a cube coordinates being in the core dictionary means the cube is on (limited to -50 <= coordinate <= 50)
+def switch_cuboid(core, comp, switch):
+    for x in range(comp[0][0], comp[0][1] + 1):
+        if not -50 <= x <= 50:
+            continue
+        for y in range(comp[1][0], comp[1][1] + 1):
+            if not -50 <= y <= 50:
+                continue
+            for z in range(comp[2][0], comp[2][1] + 1):
+                if not -50 <= z <= 50:
+                    continue
+                if switch == 'on':
+                    core[x, y, z] = 1
+                else:
+                    if (x, y, z) in core.keys():
+                        del core[x, y, z]
+
+
+# Input parsing
+with open("Day#22_Puzzle#1_Input.txt") as input_data:
+    raw = input_data.readlines()
+    input_lines = [list(map(lambda x: list(map(int, x[2:].split(".."))), line.strip().split()[-1].split(','))) for line in raw]
+    switches = [line.split()[0] for line in raw]
+
+
+# Contains coordinates of the cubes that are turned on
+reactor_core = dict()
+for i in range(len(switches)):
+    switch_cuboid(reactor_core, input_lines[i], switches[i])
+
+# Task solution1
+print(len(reactor_core), "cubes are turned on.")
